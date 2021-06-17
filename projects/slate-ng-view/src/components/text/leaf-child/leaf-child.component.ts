17dfbbc2b1c645979c4f1a7bae4875d6a72910a4
undefined <undefined>
2021-06-17 21:39:46 +0800

    refactor: delete ns-string and combine to leaf-child

--- projects/slate-ng-view/src/components/text/leaf-child/leaf-child.component.ts
+++ projects/slate-ng-view/src/components/text/leaf-child/leaf-child.component.ts
@@ -1,11 +1,11 @@
 import {
   AfterViewInit,
-  ChangeDetectionStrategy,
+  ChangeDetectionStrategy, ChangeDetectorRef,
   Component,
   Inject,
   Injector,
   OnDestroy,
-  OnInit,
+  OnInit, TemplateRef, ViewChild,
   ViewEncapsulation
 } from '@angular/core';
 import { PLACEHOLDER_SYMBOL } from '../../../utils/weak-maps';
@@ -16,9 +16,11 @@ import {
   PARENT_NODE_TOKEN,
   PLACEHOLDER_CHILDREN_TOKEN
 } from '../../element/token';
-import { Text } from 'slate';
+import {Editor, Node, Path, Text} from 'slate';
 import { PlaceholderComponent } from '../placeholder/placeholder.component';
 import { RegistryNsElement } from '../../../services/registry-ns-element.service';
+import {AngularEditor} from "../../../plugins/angular-editor";
+import {NsEditorService} from "../../../services/ns-editor.service";
 
 @Component({
   selector: 'ns-leaf-child',
@@ -31,13 +33,21 @@ export class LeafChildComponent implements OnInit, AfterViewInit, OnDestroy {
   editorEl: HTMLElement;
   placeholderPortal = null;
 
+  @ViewChild('ZeroWidthString') ZeroWidthStringTmpl: TemplateRef<any>;
+  @ViewChild('TextString') TextStringTmpl: TemplateRef<any>;
+
+  template: TemplateRef<any>;
+  context: object;
+
   constructor(
     @Inject(IS_LAST_TOKEN) readonly isLast: boolean,
     @Inject(CURRENT_NODE_TOKEN) readonly cNode: any,
     @Inject(PARENT_NODE_TOKEN) readonly pNode: any,
     @Inject(LEAF_TOKEN) readonly leaf: Text,
     private customService: RegistryNsElement,
-    private injector: Injector
+    private injector: Injector,
+    private editorService: NsEditorService,
+    private cdr: ChangeDetectorRef
   ) {
   }
 
@@ -56,6 +66,9 @@ export class LeafChildComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   ngAfterViewInit() {
+    this.generateStringData();
+    this.cdr.detectChanges();
+
     // todo: 可以重构，使用editorService
     const editorEl = this.editorEl = document.querySelector<HTMLDivElement>(
       '[data-slate-ng-editor="true"]'
@@ -72,5 +85,55 @@ export class LeafChildComponent implements OnInit, AfterViewInit, OnDestroy {
 
   ngOnDestroy() {
     this.editorEl && (this.editorEl.style.minHeight = 'auto');
+  }
+
+  private generateStringData() {
+    const editor = this.editorService.editor;
+    const path = AngularEditor.findPath(editor, this.cNode);
+    const parentPath = Path.parent(path);
+    if (editor.isVoid(this.pNode)) {
+      this.template = this.ZeroWidthStringTmpl;
+      this.context = {
+        length: Node.string(this.pNode).length,
+        isLineBreak: false
+      };
+    }
+
+    if (
+      this.leaf.text === '' &&
+      this.pNode.children[this.pNode.children.length - 1] === this.cNode &&
+      !editor.isInline(this.pNode) &&
+      Editor.string(editor, parentPath) === ''
+    ) {
+      this.template = this.ZeroWidthStringTmpl;
+      this.context = {
+        length: 0,
+        isLineBreak: true
+      };
+    }
+
+    if (this.leaf.text === '') {
+      this.template = this.ZeroWidthStringTmpl;
+      this.context = {
+        length: 0,
+        isLineBreak: false
+      };
+    }
+
+    if (this.isLast && this.leaf.text.slice(-1) === '\n') {
+      this.template = this.TextStringTmpl;
+      this.context = {
+        isTrailing: true,
+        text: this.leaf.text
+      };
+    }
+
+    if (!this.template) {
+      this.template = this.TextStringTmpl;
+      this.context = {
+        isTrailing: false,
+        text: this.leaf.text
+      };
+    }
   }
 }
--- projects/slate-ng-view/src/slate-ng-view.module.ts
+++ projects/slate-ng-view/src/slate-ng-view.module.ts
@@ -1,19 +1,18 @@
-import {NgModule} from "@angular/core";
-import {CommonModule} from "@angular/common";
-import {EVENT_MANAGER_PLUGINS} from "@angular/platform-browser";
-import {BeforeInputEventPlugin} from "./polyfills/before-input/before-input-event-plugin";
-import {EditableComponent} from "./components/editable/editable.component";
-import {PortalModule} from "@angular/cdk/portal";
-import {ChildrenComponent} from "./components/children/children.component";
-import {ElementComponent} from "./components/element/element.component";
-import {TextComponent} from "./components/text/text.component";
-import {CompositionEventPlugin} from "./polyfills/composition/composition-event-plugin";
-import {LeafComponent} from "./components/text/leaf/leaf.component";
-import {StringComponent} from "./components/text/string/string.component";
-import {PlaceholderComponent} from "./components/text/placeholder/placeholder.component";
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {EVENT_MANAGER_PLUGINS} from '@angular/platform-browser';
+import {BeforeInputEventPlugin} from './polyfills/before-input/before-input-event-plugin';
+import {EditableComponent} from './components/editable/editable.component';
+import {PortalModule} from '@angular/cdk/portal';
+import {ChildrenComponent} from './components/children/children.component';
+import {ElementComponent} from './components/element/element.component';
+import {TextComponent} from './components/text/text.component';
+import {CompositionEventPlugin} from './polyfills/composition/composition-event-plugin';
+import {LeafComponent} from './components/text/leaf/leaf.component';
+import {PlaceholderComponent} from './components/text/placeholder/placeholder.component';
 import {LeafChildComponent} from './components/text/leaf-child/leaf-child.component';
-import {RegistryNsElement} from "./services/registry-ns-element.service";
-import {NsEditorService} from "./services/ns-editor.service";
+import {RegistryNsElement} from './services/registry-ns-element.service';
+import {NsEditorService} from './services/ns-editor.service';
 import {ElementAttrsDirective} from './directives/element-attrs.directive';
 import {LeafAttrsDirective} from './directives/leaf-attrs.directive';
 import {PlaceholderAttrsDirective} from './directives/placeholder-attrs.directive';
@@ -26,7 +25,6 @@ import {PortalEntryComponent} from './components/portal-entry/portal-entry.compo
     ElementComponent,
     TextComponent,
     LeafComponent,
-    StringComponent,
     PlaceholderComponent,
     LeafChildComponent,
     PortalEntryComponent,
